2099b48965a7fa5ffd86b96d6256e51a125070a5
Kseniya <okorokova.kseniya@gmail.com>
2023-01-25 12:43:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 100;
+        int added = 1400;
+        int bonus = 0;
+        if (added > 1100) {
+            bonus = added / 100;
+        }
+
+        int finalAccount = initialAccount + added + bonus;
+
+        System.out.println("Итоговый счет: " + finalAccount);
+        System.out.println("Бонус: " + bonus);
+    }
+}
